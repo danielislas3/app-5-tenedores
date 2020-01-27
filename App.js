98df96c49bd879c9e539688b75f7a257dc9e1d98
Daniel Orio <danielislas3@live.com>
2020-01-27 12:35:56 -0600

    use state

--- App.js
+++ App.js
@@ -1,10 +1,21 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import React, {useState} from 'react'
+import { StyleSheet, Text, View, Button } from 'react-native';
+import Loading from './Lodading';
 export default function App() {
-  return (
+  const [isLoading,setIsLoading]= useState(false)
+
+  if(isLoading===true){
+    console.log('Loading: TRUE');
+  }else{
+
+    console.log('Loading: FALSE',)
+  }
+  return (  
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up Appp.js to start working on your app!</Text>
+
+      {isLoading===true && <Loading />}
+      <Button title="Actualizar estado" onPress={()=>setIsLoading(true)} ></Button>
     </View>
   );
 }
---
+++ Lodading.js
@@ -0,0 +1,11 @@
+import React, {useState} from 'react'
+import {View, Text} from 'react-native'
+
+export default function Loading(props) {
+  return (
+    <View>
+      <Text>Cargando...
+      </Text>
+    </View>
+  )
+}

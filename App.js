98d638b1af09f9142cca9336aa54508875f277dc
Daniel Orio <danielislas3@live.com>
2020-01-27 17:05:24 -0600

    RN Elements

--- App.js
+++ App.js
@@ -1,22 +1,23 @@
-import React, { useState, useEffect } from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
-import Loading from "./Lodading";
+import React from "react";
+import { Button,Icon} from 'react-native-elements';
+import { StyleSheet, Text, View } from "react-native";
+
 
 export default function App() {
-  const [isLoading, setIsLoading] = useState(false);
-
-  useEffect(() => {
-    console.log("Componente montadoo: " + isLoading);
-  }, [isLoading]);
+ 
   return (
     <View style={styles.container}>
-      <Text>Open up Appp.js to start working on your app!</Text>
-
-      {isLoading === true && <Loading />}
+      <Text>5 Tenedoress</Text>
       <Button
-        title="Actualizar estado"
-        onPress={() => setIsLoading(true)}
-      ></Button>
+       icon={
+    <Icon
+      type="font-awesome"
+      name="arrow-right"
+      size={15}
+      color="white"
+    />
+  }
+      />
     </View>
   );
 }
